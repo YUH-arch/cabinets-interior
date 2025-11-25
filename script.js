3f1a4d819402f556680c1d19af54e28b57d11643
YUH-arch <f2pma33n022715@gmail.com>
2025-11-25 15:30:04 -0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
 const container = document.querySelector('.slider-container');
+
 document.querySelector('.slider').addEventListener('input', (e) => {
   container.style.setProperty('--position', `${e.target.value}%`);
 })
+
+let mybutton = document.getElementById("myBtn");
+
+window.onscroll = function() {scrollFunction()};
+
+
+function topFunction() {
+  document.body.scrollTop = 0; 
+  document.documentElement.scrollTop = 0; 
+}
